d6d9b1c527c1cbbd20fb4763274ab76b9118fdb0
Sidakalankam <72893572+Sidakalankam@users.noreply.github.com>
2026-07-15 01:40:50 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    from collections import Counter, defaultdict
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        l = 0
+        s1_count = Counter(s1)
+        s2_count = defaultdict(int)
+
+        for r in range(len(s2)):
+            s2_count[s2[r]] += 1
+            window = r - l + 1
+
+            while window > len(s1):
+                s2_count[s2[l]] -= 1
+                if s2_count[s2[l]] == 0:
+                    del s2_count[s2[l]]
+                
+                l += 1
+                window = r - l + 1
+            
+            if s1_count == s2_count:
+                return True
+        
+        return False
